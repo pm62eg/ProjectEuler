87caef07c8bb95ceed0c22df5a02c85eefe97767
pm62eg <33402112+pm62eg@users.noreply.github.com>
2020-06-13 19:19:34 +0100

    problem 33

---
+++ p033.c
@@ -0,0 +1,57 @@
+/*
+** digit cancelling fractions
+*/
+
+/*
+** a * b = c, where a<b and #a + #b + #c = 9
+** implies #a = 2, #b = 3, #c = 4
+*/
+
+#include <stdio.h>
+
+int gcd(int a, int b) {
+    if (b == 0) return a;
+    return gcd(b, a % b);
+}
+
+int main(void) {
+    int numerator = 1;
+    int denominator = 1;
+    for (int a = 1; a < 10; a++) {
+        for (int b = a; b < 10; b++) {
+            if (b == a) continue;
+            for (int c = 1; c < 10; c++) {
+                if (c == a) continue;
+                if (c == b) continue;
+                int numac = a*10 + c;
+                int numca = c*10 + a;
+                int numbc = b*10 + c;
+                int numcb = c*10 + b;
+                if (b * numac == a * numbc) {
+                    printf("%d%d/%d%d = %d/%d\n", a, c, b, c, a, b);
+                    numerator *= a;
+                    denominator *= b;
+                }
+                if (b * numca == a * numbc) {
+                    printf("%d%d/%d%d = %d/%d\n", c, a, b, c, a, b);
+                    numerator *= a;
+                    denominator *= b;
+                }
+                if (b * numac == a * numcb) {
+                    printf("%d%d/%d%d = %d/%d\n", a, c, c, b, a, b);
+                    numerator *= a;
+                    denominator *= b;
+                }
+                if (b * numca == a * numcb) {
+                    printf("%d%d/%d%d = %d/%d\n", c, a, c, b, a, b);
+                    numerator *= a;
+                    denominator *= b;
+                }
+            }
+        }
+    }
+    int dd = gcd(numerator, denominator);
+    numerator /= dd;
+    denominator /= dd;
+    printf("product is %d/%d.\n", numerator, denominator);
+}
