0a5faf43985f68815f2f3014aa9afb86865e0c61
pm62eg <33402112+pm62eg@users.noreply.github.com>
2020-06-24 09:41:54 +0100

    simplifications

--- forth/p001.fs
+++ forth/p001.fs
@@ -9,29 +9,17 @@
 \ Find the sum of all the multiples of 3 or 5 below 1000.
 
 \ determine if n is a multiple of 3 or 5
-: e001-m35?      (     n -- flag )
-    dup          (   n n --      )
-    3 mod        ( n n%3 --      )
-    0 = if       (     n --      )
-        drop -1  (       -- flag ) \ it is a multiple of 3
-    else         (     n --      )
-        5 mod    (   n%5 --      )
-        0 = if
-            -1   (       -- flag ) \ it is a multiple of 5
-        else
-            0    (       -- flag ) \ not a multiple of either 3 or 5
-        then
-    then ;
+: e001-m35? ( n -- flag )
+    dup
+    3 mod 0= if exit then \ leave n (true) in the stack
+    5 mod 0= ;            \ leave test in the stack
 
 \ sum multiples of 3 or 5 up to, excluding, n
-: e001-summ35           (     n -- sum )
-    0 swap              ( sum n --     ) \ 0 to start sum
-    1 do                                \ loop from 1 to n
-        i e001-m35? if  (   sum --     )
-            i           ( sum i --     )
-            +           (   sum --     )
-        then            (   sum --     )
-    loop ;              (       -- sum )
+: e001-summ35 ( n -- sum )
+    0 swap 1 do             \ loop from 1 to n, add to TOS
+        i e001-m35? if
+            i + then        \ add i when true
+    loop ;
 
 : euler001
     1000 e001-summ35 . ;
