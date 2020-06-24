af934f1e1288cb4e3c677ada5f448fd5a5912a8e
pm62eg <33402112+pm62eg@users.noreply.github.com>
2020-06-24 11:02:09 +0100

    simplifications

--- forth/p002.fs
+++ forth/p002.fs
@@ -11,30 +11,17 @@
 \ By considering the terms in the Fibonacci sequence whose values
 \ do not exceed four million, find the sum of the even-valued terms.
 
-\ accumulate top 2 values into the 3rd, leave stack ready for next
-: e002-nextfib     ( a b -- b a+b )
-    swap over + ;
-
 \ calculate next fib, update accum if needed
-: e002-helper           (       accum a b -- ACCUM b a+b       )
-    e002-nextfib        (       accum a b -- accum b a+b       )
-    dup 2 mod if        (       accum a b -- accum b a+b flag  )
-    else
-        rot             (     accum b a+b -- b a+b accum       )
-        over            (     b a+b accum -- a a+b accum a+b   )
-        +               ( b a+b accum a+b -- b a+b accum[+a+b] )
-        rot rot         (                 -- accum[+a+b] b a+b )
-    then ;
+: e002-nextaddeven ( accum a b -- accum b a+b )
+    tuck +                     \ next fibonacci
+    dup 2 mod 0= if            \ if even
+        rot over + -rot then ; \ add to accum
 
 \ evaluate fibonacci, update accum, and test if limit is reached
-: e002-mainloop
-    begin
-        e002-helper     ( limit accum a b -- limit ACCUM A B       )
-        3 pick          ( limit accum a b -- limit accum a b LIMIT )
-        over <          (             ... -- limit accum a b FLAG  )
-    until ;             (             ... -- limit accum a b       )
+: e002-mainloop ( limit accum a b -- limit accum a b )
+    begin e002-nextaddeven      \ calculate [and add] next fibonacci
+          3 pick over < until ; \ check for limit
 
 : euler002
-    4000000 0 1 1           ( limit accum a b --                   )
-    e002-mainloop           ( limit accum a b -- limit ACCUM A B   )
-    drop drop swap drop . ; \ print accum, clear stack
+    4000000 0 1 1 e002-mainloop
+    drop drop . drop ;  \ print accum, clear stack
