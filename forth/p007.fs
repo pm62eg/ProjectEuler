05bcecf9d6fcb1b1f2352b265521f7992be5cc0e
pm62eg <33402112+pm62eg@users.noreply.github.com>
2020-06-25 18:40:11 +0100

    simplify, refactor

--- forth/p007.fs
+++ forth/p007.fs
@@ -6,31 +6,19 @@
 
 \ What is the 10 001st prime number?
 
-: e007-prime? ( n -- f )
-         dup 2 = if      drop true
-    else dup 2 mod 0= if drop false
-    else 3
-        begin 2dup dup * >=
-        while 2dup mod 0=
-              if       2drop false exit
-              then
-              2 +
-        repeat
-        2drop true
-    then then ;
+\ no need to check for 2 or even numbers
+: e007-prime?
+    3 ( n factor ) begin 2dup dup * >= while
+                   2dup mod 0= if 2drop false exit then 2 + repeat
+    2drop true ;
 
-: e007-nthprime               ( n -- p )
-    dup 1 = if
-        drop 2                \ 2 is the 1st prime
-    else
-       1 -                    \ 2 is already counted
-       1 begin                ( n prev-candidate -- n[-1] curr-candidate )
-           2 +                \ update curr-candidate
-           dup e007-prime? if
-               swap 1 - swap  \ if prime update n
-           then
-       over 0= until
-    then ;
+: e007-nthoddprime
+    1 ( n prev ) begin 2 + dup e007-prime? if swap 1 - swap then
+                 over 0= until nip ;
 
-: euler007
-    10001 e007-nthprime . drop ;
+\ the 1st prime is 2, otherwise calculate the n-1th odd prime
+: e007-nthprime
+    assert( dup 1 >= )
+    dup 1 = if drop 2 exit then 1 - e007-nthoddprime ;
+
+: euler007 10001 e007-nthprime . ;
