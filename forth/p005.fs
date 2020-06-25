b95827d5f2efd50ca1c2b8b642130f2060d2230c
pm62eg <33402112+pm62eg@users.noreply.github.com>
2020-06-25 16:35:56 +0100

    reformat

--- forth/p005.fs
+++ forth/p005.fs
@@ -7,21 +7,6 @@
 \ What is the smallest positive number that is evenly divisible
 \ by all of the numbers from 1 to 20?
 
-: e005-gcd ( a b -- n )
-    begin
-        dup             ( a b -- a b b )
-        while           (     -- a b   )
-        tuck            (     -- b a b )
-        mod             (     -- b a%b )
-    repeat
-    drop ;              (     -- a     )
-
-: e005-lcm ( a b -- n )
-    over over *
-    -rot e005-gcd / ;
-
-: euler005
-    1                         \ lcm of 1 and whatever
-    21 2 do \ 2, 3, ..., 20
-        i e005-lcm            \ lcm of 1 and 2, and 3, and 4, ...
-    loop . ;
+: e005-gcd begin dup while tuck mod repeat drop ;
+: e005-lcm over over * -rot e005-gcd / ;
+: euler005 1 ( result ) 21 2 do i e005-lcm loop . ;
