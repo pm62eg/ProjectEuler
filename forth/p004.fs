d93a3748b14b223fffaaf678265d7cd3a613a4ad
pm62eg <33402112+pm62eg@users.noreply.github.com>
2020-06-25 16:28:18 +0100

    reformat, mostly

--- forth/p004.fs
+++ forth/p004.fs
@@ -7,38 +7,17 @@
 
 \ Find the largest palindrome made from the product of two 3-digit numbers.
 
-: e004-digit ( n d -- n[d] )
-    begin
-        dup 0 > while
-        swap 10 / swap 1 -
-    repeat
-    drop 10 mod ;
+: e004-nthdigit ( n d -- n[d] )
+    begin dup 0 > while swap 10 / swap 1 - repeat drop 10 mod ;
 
 : e004-palindrome6? ( 6-digit-n -- flag )
-    dup 0 e004-digit       ( n -- n d0 )
-    over 5 e004-digit      ( n d0 -- n d0 d5 )
-    = if                ( n -- )
-        dup 1 e004-digit
-        over 4 e004-digit
-        = if
-            dup 2 e004-digit
-            over 3 e004-digit
-            = if
-            else
-                0 *
-            then
-        else
-            0 *
-        then
-    else
-        0 *             \ not palindrome
-    then ;
+    assert( dup 100000 >= )
+    assert( dup 999999 <= )
+    dup 0 e004-nthdigit over 5 e004-nthdigit <> if drop false exit then
+    dup 1 e004-nthdigit over 4 e004-nthdigit <> if drop false exit then
+    dup 2 e004-nthdigit swap 3 e004-nthdigit = ;
 
 : euler004
-    900 999 do
-        900 i do
-            i j *
-            dup e004-palindrome6?
-            if . else drop then
-        -loop
-    -loop ;
+    900 999 do 900 i do
+        i j * dup e004-palindrome6? if . else drop then
+    -1 +loop -1 +loop ;
